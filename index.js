f4966f40c057846920a48ed3a58149f412b3171b
fuad1706 <fuadshorlarxwix@gmail.com>
2024-02-03 21:28:08 -0800

    script

---
+++ index.js
@@ -0,0 +1,18 @@
+let myName = "Abracadabra";
+console.log(myName.length);
+
+let thisBoy = "Come to me and " + "and let us merry ";
+console.log(thisBoy)
+
+let plusEqual = "This came to me ";
+plusEqual += "very sudden hmmm";
+console.log(plusEqual);
+
+let myNames = "Adebakin Fuad ";
+let otherSent = "My Full name is " + myNames +" dont play with me ";
+console.log(otherSent); 
+
+let aNoun = "Tobi ";
+let aVerb = "Fought well!";
+aNoun += aVerb
+console.log(aNoun);
